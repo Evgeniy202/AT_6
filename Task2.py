c10d3b3a38fe7635f4d56c6fd9b6c97738cde6e4
Yevgenii Krotko <87773197+Evgeniy202@users.noreply.github.com>
2023-11-20 22:13:51 +0200

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def is_symmetric(root):
+    def is_mirror(left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        return left.val == right.val and is_mirror(left.left, right.right) and is_mirror(left.right, right.left)
+
+    return is_mirror(root, root)
+
+
+root1 = TreeNode(1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(2, TreeNode(4), TreeNode(3)))
+print(is_symmetric(root1))
+
+root2 = TreeNode(1, TreeNode(2, None, TreeNode(3)), TreeNode(2, None, TreeNode(3)))
+print(is_symmetric(root2))
+
+root3 = TreeNode(1, TreeNode(2), TreeNode(1))
+print(is_symmetric(root3))
